c115e39b2ee744bcc331d1b00ae3eab052a930b2
Manish Vasani <mavasani@microsoft.com>
2016-08-03 15:54:50 -0700

    Willow work

--- SetDevCommandPrompt.cmd
+++ SetDevCommandPrompt.cmd
@@ -1,9 +1,16 @@
 @echo off
 
+if exist "%VSINSTALLDIR%" goto done
+
 :: prefer building with Dev15
 
 set CommonToolsDir=%VS150COMNTOOLS%
 if not exist "%CommonToolsDir%" set CommonToolsDir=%VS140COMNTOOLS%
 if not exist "%CommonToolsDir%" exit /b 1
 
+pushd
 call "%CommonToolsDir%\VsDevCmd.bat"
+popd
+
+:done
+exit /b 0
