cef3781fdb6283017ee7b2034865fbfb469dc0ca
Manish Vasani <mavasani@microsoft.com>
2020-07-31 15:51:59 -0700

    Use CWT

--- src/Compilers/Core/Portable/DiagnosticAnalyzer/CachingSemanticModelProvider.cs
+++ src/Compilers/Core/Portable/DiagnosticAnalyzer/CachingSemanticModelProvider.cs
@@ -6,34 +6,39 @@
 
 using System.Collections.Concurrent;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace Microsoft.CodeAnalysis.Diagnostics
 {
     internal sealed class CachingSemanticModelProvider : SemanticModelProvider
     {
-        private readonly ConcurrentDictionary<SyntaxTree, SemanticModel> _semanticModelsMap;
+        private readonly ConditionalWeakTable<SyntaxTree, SemanticModel> _semanticModelsCache;
 
         public CachingSemanticModelProvider()
         {
-            _semanticModelsMap = new ConcurrentDictionary<SyntaxTree, SemanticModel>();
+            _semanticModelsCache = new ConditionalWeakTable<SyntaxTree, SemanticModel>();
         }
 
         public override SemanticModel GetSemanticModel(SyntaxTree tree, Compilation compilation)
         {
-            if (_semanticModelsMap.TryGetValue(tree, out var model))
+            if (_semanticModelsCache.TryGetValue(tree, out var model))
             {
                 Debug.Assert(model.Compilation == compilation);
                 return model;
             }
 
-            // Avoid infinite recursion by passing 'useSemanticModelProviderIfNonNull: false'
-            model = compilation.GetSemanticModel(tree, ignoreAccessibility: false, useSemanticModelProviderIfNonNull: false);
-            return _semanticModelsMap.GetOrAdd(tree, model);
+            return _semanticModelsCache.GetValue(tree, createSemanticModel);
+
+            SemanticModel createSemanticModel(SyntaxTree tree)
+            {
+                // Avoid infinite recursion by passing 'useSemanticModelProviderIfNonNull: false'
+                return compilation.GetSemanticModel(tree, ignoreAccessibility: false, useSemanticModelProviderIfNonNull: false);
+            }
         }
 
         internal void RemoveCachedSemanticModel(SyntaxTree tree)
         {
-            _semanticModelsMap.TryRemove(tree, out _);
+            _semanticModelsCache.Remove(tree);
         }
     }
 }
